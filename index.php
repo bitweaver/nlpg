0800790ae36de7529621b0fe646aebb86ca56c4e
Lester Caine <lester@lsces.co.uk>
2008-08-27 16:23:38 +0000

    Copy updated index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * This source code is distributed under the terms as layed out in the
+ * GNU General Public License.
+ *
+ * Purpose: To provide the main entry point in accessing an NLPG repository entry
+ *
+ * Ported to bitweaver framework by Lester Caine 2006-12-29
+ * @version $Id: index.php,v 1.2 2008/08/27 16:23:38 lsces Exp $
+ */
+
+// Initialization
+require_once( '../bit_setup_inc.php' );
+
+if( !empty( $_REQUEST['usrn'] ) ) {
+	header( "location: ".NLPG_PKG_URL."display_street.php?usrn=".( ( int )$_REQUEST['usrn'] ) );
+}
+
+if( !empty( $_REQUEST['uprn'] ) ) {
+	header( "location: ".NLPG_PKG_URL."display_property.php?uprn=".( ( int )$_REQUEST['uprn'] ) );
+}
+
+if( !empty( $_REQUEST['c_id'] ) ) {
+	header( "location: ".NLPG_PKG_URL."list_county.php?sort_mode=c_id_asc&list=local&c_id=".$_REQUEST['c_id'] );
+}
+
+if( !empty( $_REQUEST['l_id'] ) ) {
+	header( "location: ".NLPG_PKG_URL."list_county.php?sort_mode=l_id_asc&list=ward&l_id=".$_REQUEST['l_id'] );
+}
+
+$gBitSystem->display( 'bitpackage:nlpg/list_entries.tpl', tra( 'Extract status for: ' ) );
+?>

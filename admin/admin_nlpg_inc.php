a76a13c3c848088ef0bb112b0d85f909c1b0a5a0
Lester Caine <lester@lsces.co.uk>
2008-09-01 12:36:25 +0000

    Start including nlpg admin facilities

--- admin/admin_nlpg_inc.php
+++ admin/admin_nlpg_inc.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version $Header: /cvsroot/bitweaver/_bit_nlpg/admin/admin_nlpg_inc.php,v 1.1 2008/08/27 16:26:17 lsces Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_nlpg/admin/admin_nlpg_inc.php,v 1.2 2008/09/01 12:36:25 lsces Exp $
  *
  * @author lsces
  * @package nlpg
@@ -13,31 +13,31 @@
 $formNlpgDisplayOptions = array(
   // This should pobably be a generic setting in kernel or something that is
   // site wide for all html_select_times and such.
-	"events_use_24" => array(
-		'label' => 'Use 24 Hour',
-		'note' => 'Time display uses 24 hour format.',
+	"nlpg_maplink" => array(
+		'label' => 'Include links to map pages',
+		'note' => 'Expand coordinate displays to include links to map packages. The maps packagas available are defined separatly.',
 		'type' => 'toggle',
 	),
-	"events_end_year" => array(
-		'label' => 'Events End Year',
-		'note' => 'End year in events date set. Can be a specific year or +# (i.e. +1) to allow events a certain number of years in the future.',
+	"nlpg_default_ordering" => array(
+		'label' => 'Initial ordering of results',
+		'note' => 'Used to supply the initial display ordering.',
 		'type' => 'input',
 	),
 );
 $gBitSmarty->assign( 'formNlpgDisplayOptions', $formNlpgDisplayOptions );
 
 $formNlpgFeatureOptions = array(
-	"events_moderation" => array(
-		'label' => 'Events Moderation',
-		'note' => 'Use content status to moderate events.',
+	"nlpg_edit" => array(
+		'label' => 'On-line editing',
+		'note' => 'Enable on-line edit pages for NLPG information. Additional to edit permission controls to allow finer management.',
 		'type' => 'toggle',
 	),
 );
 $gBitSmarty->assign( 'formNlpgFeatureOptions', $formNlpgFeatureOptions );
 
 if( !empty( $_REQUEST['nlpg_preferences'] ) ) {
-  	$events = array_merge( $formNlpgDisplayOptions, $formNlpgFeatureOptions  );
-	foreach( $events as $item => $data ) {
+  	$nlpg_opt = array_merge( $formNlpgDisplayOptions, $formNlpgFeatureOptions  );
+	foreach( $nlpg_opt as $item => $data ) {
 		if( $data['type'] == 'numeric' ) {
 			simple_set_int( $item, NLPG_PKG_NAME );
 		} elseif( $data['type'] == 'toggle' ) {

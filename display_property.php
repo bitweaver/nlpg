2e770bcd326aa3117af55d3702d530f015ceb757
wjames5 <will@tekimaki.com>
2009-10-01 14:17:02 +0000

    remove ref to non-existant copyright.txt file

--- display_property.php
+++ display_property.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_nlpg/display_property.php,v 1.3 2009/10/01 13:45:45 wjames5 Exp $
+ * $Header: /cvsroot/bitweaver/_bit_nlpg/display_property.php,v 1.4 2009/10/01 14:17:02 wjames5 Exp $
  *
  * Copyright (c) 2006 bitweaver.org
- * All Rights Reserved. See copyright.txt for details and a complete list of authors.
+ * All Rights Reserved. See below for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details.
  *
  * @package nlpg

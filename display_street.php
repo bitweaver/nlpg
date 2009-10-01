bcf57c6c84301ac7aa5f7f2693f847659c89b6e3
wjames5 <will@tekimaki.com>
2009-10-01 13:45:45 +0000

    fix LGPL license ref from non-existant license.txt file to lgpl url

--- display_street.php
+++ display_street.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_nlpg/display_street.php,v 1.2 2008/09/01 12:32:16 lsces Exp $
+ * $Header: /cvsroot/bitweaver/_bit_nlpg/display_street.php,v 1.3 2009/10/01 13:45:45 wjames5 Exp $
  *
  * Copyright (c) 2006 bitweaver.org
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
- * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details.
  *
  * @package nlpg
  * @subpackage functions
